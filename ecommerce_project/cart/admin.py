2f359472f4156ff47daf615a6327bd874844dc2f
Vishnu  V S <105106551+Vishnu7025@users.noreply.github.com>
2022-10-03 15:53:47 +0530

    Delete ecommerce_project directory

--- ecommerce_project/cart/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Cart,CartItem, Review
-# Register your models here.
-admin.site.register(Cart)
-admin.site.register(CartItem)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display =['id','user','product','rate','created_at']
-    readonly_fields = ['created_at',]
-admin.site.register(Review,ReviewAdmin)
